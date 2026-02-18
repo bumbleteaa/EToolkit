88152490fac90cf3fc61d8f4c0f168d82e999ff9
bumbleteaa <lukitrisetiawan2505@gmail.com>
2026-02-18 07:36:04 -0500

    allowing nullable properties in <CsvComponentPlacementRow>

--- EToolkit/Infrastructure/CsvComponentPlacementRow.cs
+++ EToolkit/Infrastructure/CsvComponentPlacementRow.cs
@@ -1,16 +1,16 @@
 ﻿namespace EToolkit.Infrastructure;
-// TODO Non nullable reference type for CsvComponentPlacementRow, we can use record struct if we want to make it immutable, or we can use class with init property if we want to make it mutable. For now, we will use class with init property for simplicity, and we can change it to record struct later if needed.
+
 public class CsvComponentPlacementRow
 {
-    public string comp { get; set; }
-    public string FeederId { get; set; }
-    public string Nozzle { get; set; }
-    public string Name { get; set; }
-    public string Value { get; set; }
-    public string Footprint { get; set; }
+    public string? comp { get; set; }
+    public string? FeederId { get; set; }
+    public string? Nozzle { get; set; }
+    public string? Name { get; set; }
+    public string? Value { get; set; }
+    public string? Footprint { get; set; }
     public double X { get; set; }
     public double Y { get; set; }
     public double Rotation { get; set; }
-    public string Desc { get; set; }
-    public string Side { get; set; }
+    public string? Desc { get; set; }
+    public string? Side { get; set; }
 }
